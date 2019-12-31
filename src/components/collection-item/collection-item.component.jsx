1114655cfa9f49a0dc9269029f63cd9ea14bd3fa
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-12-31 09:53:45 -0500

    created cart-item component and connected our cart dropdown with cartItems reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -25,7 +25,7 @@ const CollectionItem = ({ item, addItem }) => {
 }
 
 const mapDispatchToProps = dispatch => ({
-  addItem: item => dispatch(addItem(addItem))
+  addItem: item => dispatch(addItem(item))
 })
 
 export default connect(
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
-import CartActionTypes from './cart.types'
-import { addItemToCart } from './cart.utils'
+import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -11,13 +11,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
-        hidden:!state.hidden
-      }
-      case CartActionTypes.ADD_ITEM:
+        hidden: !state.hidden
+      };
+    case CartActionTypes.ADD_ITEM:
         return {
           ...state,
           cartItems: addItemToCart(state.cartItems, action.payload)
-        }
+        };
       default:
         return state
   }
